87c6fefc024c1a98f9d9c71d59a5574cf6873249
eleahburman <eleahjburman@gmail.com>
2023-10-20 14:19:45 -0400

    Add loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+//Routes
+import Dashboard, { dashboardLoader } from "./pages/Dashboard";
 
 function App() {
-  
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Dashboard />,
+      loader: dashboardLoader
+    },
+  ]);
 
   return (
       <div className="App">
-        
+        <RouterProvider router={router} />
       </div>
   )
 }
