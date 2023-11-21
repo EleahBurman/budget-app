1db55bc2a5456043c499a4686394a45cc37d8b30
eleahburman <eleahjburman@gmail.com>
2023-11-21 11:21:38 -0500

    Separate routes and controllers in budgets

--- routes/budgetRoutes.js
+++ routes/budgetRoutes.js
@@ -1,71 +1,14 @@
-import express from 'express'
-import { Budget, Expense } from '../models/index.js';
+import express from 'express';
+import { getBudgets, createBudget, getBudgetById, deleteBudget } from '../controllers/budgetControllers.js';
 
 const budgetRouter = express.Router();
 
-budgetRouter
-  .route("/")
-  .get(async (req, res)=>{
-    const budgets = await Budget.find().populate({
-      path: 'expenses',
-      model: 'Expense'
-  })
-    res.json(budgets)
-  })
-  .post( async (req, res)=>{
-    const {name, amount, color} = req.body
-    try{
+budgetRouter.route("/")
+  .get(getBudgets)
+  .post(createBudget);
 
-        const result = await Budget.create({
-          name,
-          amount,
-          color
-        })
-        res.json(result)
+budgetRouter.route("/:budgetId")
+  .get(getBudgetById)
+  .delete(deleteBudget);
 
-    } catch (err){
-      console.log(err);
-      res.json({
-          message: err
-      })
-    }
-  })
-  budgetRouter.route("/:budgetId")
-    .get(async (req, res)=>{
-      const budgetId = req.params.budgetId;
-
-      Budget.findById(budgetId).populate({
-        path: 'expenses',
-        model: 'Expense'
-      })
-      .then( (budget) => {
-        res.json(budget)
-      })
-
-    })
-    .delete(async (req, res)=>{
-      const budgetId = req.params.budgetId;
-      const associatedExpenses = await Budget.findById(budgetId).populate('expenses')
-      console.log(associatedExpenses, "this is associated expenses")
-      if(associatedExpenses.expenses.length > 1){
-        let deletedExpenses = await Expense.deleteMany({ _id: { $in: associatedExpenses.expenses } })
-        console.log(deletedExpenses, "this is deleted expenses")
-      }else if(associatedExpenses.expenses.length === 1){
-        let deletedExpenses = await Expense.deleteOne({ _id: { $in: associatedExpenses.expenses } })
-        console.log(deletedExpenses, "this is deleted expenses when one expense is left")
-      } else{
-        console.log("no expenses to delete")
-      }
-      console.log("are we deleting anything in here", budgetId)
-      Budget.deleteOne({_id:budgetId})
-      .then( (response) => {
-        console.log(response, "whats the response")
-        res.end()
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-
-    })
-
-export default budgetRouter
+export default budgetRouter;
