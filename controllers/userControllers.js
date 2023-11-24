fcbef8b977ba8e1cc7a4f22e72d9ea4b476d955a
eleahburman <eleahjburman@gmail.com>
2023-11-24 13:08:39 -0500

    Hash password functionality

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,18 +1,53 @@
+import { User } from '../models/index.js';
+import bcrypt from 'bcryptjs';
+
 //@desc Register a user
 //@route POST /api/users/register
 //@access Public
 
-export const registerUser = async (req, res) => {
-  try {
-    res.json({
-      message: 'Register the user'
-    });
-  } catch (err) {
-    console.log(err);
-    res.json({
-      message: err
-    });
+export const registerUser = (req, res) => {
+  console.log("This is req body: ", req.body);
+
+  const {username, email, password} = req.body;
+
+  if(!username || !email || !password) {
+    return res.status(400).json({ message: 'Please fill in all fields' });
   }
+
+  User.findOne({email})
+    .then(userAvailable => {
+      if (userAvailable) {
+        return res.status(400).json({ message: 'User already exists' });
+      }
+
+      //Hash password
+      return bcrypt.hash(password, 10);
+    })
+    .then(hashedPassword => {
+      console.log("Hashed Password: ", hashedPassword);
+      return User.create({
+        username,
+        email,
+        password: hashedPassword
+      });
+    })
+    .then(user => {
+      console.log(`User created ${user}`);
+      if (user){
+        res.status(201).json({
+          _id: user.id,
+          email: user.email,
+        });
+      } else {
+        res.status(400).json({ message: 'Invalid user data' });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        message: err.message
+      });
+    });
 };
 
 //@desc Login a user
--- models/userSchema.js
+++ models/userSchema.js
@@ -1,28 +1,25 @@
 import mongoose from "mongoose";
-
 let Schema = mongoose.Schema;
 
 let userSchema = new Schema({
   username: {
     type: String,
     required: [true, "Please enter a username"],
-    unique: [true, "Username already exists"],
+    // unique: [true, "Username already exists"],
     minlength: 5,
     maxlength: 20
   },
   email: {
     type: String,
     required: [true, "Please enter an email address"],
-    unique: [true, "Email already exists"],
+    // unique: [true, "Email already exists"],
     minlength: 5,
     maxlength: 20
   },
   password: {
     type: String,
-    unique: [true, "Password already exists"],
     required: [true, "Please enter a password"],
-    minlength: 5,
-    maxlength: 20
+    minlength: 5
   },
 },
   {
