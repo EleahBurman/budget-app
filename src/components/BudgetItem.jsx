223f0be5fba1d088f47bc7559fe3dd0ac9b0d970
eleahburman <eleahjburman@gmail.com>
2024-02-08 14:47:12 -0500

    Add custom modal to delete budget

--- src/components/BudgetItem.jsx
+++ src/components/BudgetItem.jsx
@@ -1,5 +1,8 @@
 //rrd imports
-import {Form, Link} from "react-router-dom"
+import {Form, Link, useNavigate } from "react-router-dom"
+
+//react imports
+import { useState } from "react";
 
 //component import
 import ExpensePieChart from "../components/ExpensePieChart";
@@ -9,10 +12,49 @@ import { BanknotesIcon, TrashIcon } from "@heroicons/react/24/outline";
 
 //helper functions
 import { formatCurrency, formatPercentages } from "../helpers";
+import { toast } from "react-toastify";
+import Modal from 'react-modal';
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)',
+    backgroundColor       : 'white',
+    color                 : 'black',
+    borderRadius          : '10px',
+    border                : '1px solid #1bbbc3',
+    boxShadow             : '0 0 8px 0 #1bbbc3',
+  }
+};
 
 const BudgetItem = ({budget, expenses, showDelete, showChart}) => {
+  const navigate = useNavigate();
   const {_id, name, amount, totalSpent} = budget;
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
 
+  const handleDelete = async (event) => {
+    event.preventDefault();
+    const result = await fetch(`/api/budgets/${budget._id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  
+    if (result.ok){
+      navigate('/');
+      toast.success(`User ${budget.name} deleted successfully`);
+    } else {
+      const errorData = await result.text();
+      console.error("Error deleting budget item:", errorData);
+      toast.error("Error deleting budget item");
+    }
+    setShowDeleteModal(false);
+  };
 
   return (
     <div 
@@ -35,24 +77,41 @@ const BudgetItem = ({budget, expenses, showDelete, showChart}) => {
       {
         showDelete ? (
           <div className="flex-sm">
-            <Form 
-              method="post"
-              action="delete"
-              onSubmit={(event) => {
-                if(!confirm("Are you sure you want to permanently delete this budget?"))
-                  event.preventDefault();
-              }}
+            <button
+              type="submit"
+              className="btn"
+              onClick={() => setShowDeleteModal(true)}
             >
-              <button
-                type="submit"
-                className="btn"
-              >
-                <span>Delete Budget</span>
-                <TrashIcon width={20} />
-              </button>
-            </Form>
+              <span
+                style={{marginRight:"10px"}}>Delete Budget</span>
+              <TrashIcon width={20} />
+            </button>
             <ExpensePieChart expenses={expenses} />
-          </div>
+          
+            <Modal
+              isOpen={showDeleteModal}
+              onRequestClose={() => setShowDeleteModal(false)}
+              style={customStyles}
+              contentLabel="Delete Budget Item Modal"
+            >
+              <h2 style={{fontSize: "30px", color: "#1bbbc3"}}>Confirm Delete Budget</h2>
+              <p style={{marginBottom: "10px" }}>Are you sure you want to permanently delete <span style={{fontWeight: "bold", color: "#1bbbc3"}}>{budget.name}</span>?</p>
+              <Form
+                onSubmit={handleDelete}
+                method="post"
+                action="delete">
+                <div className="button-container">
+                  <button type="submit" className="btn btn--dark"  style={{marginRight: "10px"}}>
+                    Delete 
+                    <TrashIcon width={20} />
+                  </button>
+                  <button type="button" className="btn btn--dark" onClick={() => setShowDeleteModal(false)}>
+                  Cancel
+                  </button>
+                </div>
+              </Form>
+            </Modal>
+        </div>
         ) : (
           <div className="flex-sm">
             <Link
